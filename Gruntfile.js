b5ce10a2ee64f7215d0c602d521134f12f355aa3
Open-Steps.org <contact@open-steps.org>
2014-09-18 17:39:56 +0200

    implemented getProfiles
    function returns the the list of profiles listed by the directory in
    json.

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,7 +19,6 @@ module.exports = function(grunt){
 
   grunt.loadNpmTasks('grunt-develop');
   grunt.loadNpmTasks('grunt-contrib-watch');
-
   grunt.registerTask('default', ['develop', 'watch']);
 
 };
--- daemon.js
+++ daemon.js
@@ -6,16 +6,17 @@ var uuid = require('uuid');
 var _ = require('lodash');
 
 var daemon = express();
-daemon.use(bodyParser.json());
+daemon.use(bodyParser.json())
 
 // listProfile: Receives the URI (probably from a plp-editor) and adds it to the list
 daemon.post('/listProfile', function(req, res){
 
-  // Get the URI where the profile is stored
-  var profileUri = req.data;
+  // Get the URI where the profile is stored from requesr
+
+  var profileUri = req.body.uri;
 
   // Assign uuid and store in FS
-  var path = 'data/listings'+uuid.v4();
+  var path = 'data/listings/'+ uuid.v4();
 
   fs.writeFile(path,profileUri, function (err) {
 
@@ -38,7 +39,23 @@ daemon.post('/listProfile', function(req, res){
 // getProfiles: Returns the profiles listed on this plp-directory
 daemon.get('/getProfiles', function(req, res){
 
-  // Loop through the profile folder and return URIs
+  // Create object to store profile URIs
+  var profiles = new Object();
+
+  // Loop through the listings folder and append URIs to be returned
+  var files = fs.readdirSync('data/listings/');
+
+  files.forEach( function (file) {
+
+    var data = fs.readFileSync('data/listings/'+file,{encoding:"utf8"});
+
+    // Append the URI of the profile on the profiles Object.
+    profiles[file] = data;
+
+  });
+
+  // Once all files have been looped, return the JSON object
+  res.send(200,JSON.stringify(profiles));
 
 });
 
