9e0efab1df2c911361817b0d44a6967364f71a77
Open-Steps.org <contact@open-steps.org>
2014-10-26 22:52:34 +0100

    Fixes #9

--- daemon.js
+++ daemon.js
@@ -19,92 +19,123 @@ daemon.options('*', cors());
 // listProfile: Receives the URI (probably from a plp-editor) and adds it to the list
 daemon.post('/', function(req, res){
 
-  // Get the URI where the profile is stored from requesr
-  var profile = req.body;
-  var uuid = UUID.v4();
-  var uri = 'http://' + config.domain + '/' + uuid;
-  var path = 'data/'+ uuid;
+	// Get the URI where the profile is stored from requesr
+	var profile = req.body;
+	if (!profile) res.send(500);
 
-  superagent.get(profile['@id'])
-    .accept('application/ld+json')
-    .buffer()
-    .end(function(provRes){
-      if(provRes.ok){
+	var uuid = UUID.v4();
+	var uri = 'http://' + config.domain + '/' + uuid;
+	var path = 'data/'+ uuid;
 
-        var fullProfile = JSON.parse(provRes.text);
+	fs.readdir(config.dataPath, function(err, files){
+		if(err) return console.log( "readdir failed.", err);
 
-        // delete context FIXME
-        delete fullProfile["@context"];
+		//remove .gitkeep
+		_.remove(files, function(name){ return name === ".gitkeep"; });
+		console.log(files.map(readFile));
 
-        var listing = {
-          "@id": uri,
-          "@type": "Listing",
-          about: fullProfile
-        };
+		Promise.all(files.map(readFile)).then(function(listings) {
 
-        // Save to filesystem
-        fs.writeFile(path, JSON.stringify(listing), function (err) {
+			for (listing in listings){
 
-          if (err) {
-            console.log(err);
-            res.send(500);
-            throw err;
-          }
+				if (listing["about"]["@id"] == profile["@id"]){
 
-          console.log('Saved profile to:', path);
+					uuid = listing["@id"].split("/").pop();
+					uri = 'http://' + config.domain + '/' + uuid;
+					path = 'data/'+ uuid;
+					break;
+				}
 
-          // Reference to this listing should be appended to the profile before returning it
-          var tiny = {
-            "@context": "http://plp.hackers4peace.net/context.jsonld",
-            "@id": uri,
-            "@type": "Listing"
-          };
+			}
 
-          res.json(tiny);
+			superagent.get(profile['@id'])
+				.accept('application/ld+json')
+				.buffer()
+				.end(function(provRes){
+					if(provRes.ok){
 
-        });
-      } else {
-        console.log('failed fetching', profile["@id"]);
-      }
-    });
+						var fullProfile = JSON.parse(provRes.text);
+
+						// delete context FIXME
+						delete fullProfile["@context"];
+
+						var listing = {
+							"@id": uri,
+							"@type": "Listing",
+							about: fullProfile
+						};
+
+						// Save to filesystem
+						fs.writeFile(path, JSON.stringify(listing), function (err) {
+
+							if (err) {
+								console.log(err);
+								res.send(500);
+								throw err;
+							}
+
+							console.log('Saved profile to:', path);
+
+							// Reference to this listing should be appended to the profile before returning it
+							var tiny = {
+								"@context": "http://plp.hackers4peace.net/context.jsonld",
+								"@id": uri,
+								"@type": "Listing"
+							};
+
+							res.json(tiny);
+
+						});
+					} else {
+						console.log('failed fetching', profile["@id"]);
+					}
+				});
+
+			console.log("Done reading files. returning " + result);
+
+		}).catch(function(err){
+			console.log( "readdir failed.", err);
+		});
+	});
+
 });
 
 // returns a promise which resolves with JSON object from file
 function readFile(name){
-  return new Promise(function(resolve, reject){
-    fs.readFile(config.dataPath + name, function(err, buffer){
-      if(err) reject(err);
-      resolve(JSON.parse(buffer.toString()));
-    });
-  });
+	return new Promise(function(resolve, reject){
+		fs.readFile(config.dataPath + name, function(err, buffer){
+			if(err) reject(err);
+			resolve(JSON.parse(buffer.toString()));
+		});
+	});
 }
 
 // getProfiles: Returns the profiles listed on this plp-directory
 daemon.get('/', function(req, res){
 
-  fs.readdir(config.dataPath, function(err, files){
-    if(err) return console.log( "readdir failed.", err);
+	fs.readdir(config.dataPath, function(err, files){
+		if(err) return console.log( "readdir failed.", err);
 
-    //remove .gitkeep
-    _.remove(files, function(name){ return name === ".gitkeep"; });
-  console.log(files.map(readFile));
+		//remove .gitkeep
+		_.remove(files, function(name){ return name === ".gitkeep"; });
+	console.log(files.map(readFile));
 
-    Promise.all(files.map(readFile)).then(function(listings) {
-      var result = {
-        "@context": CONTEXT_URI,
-        "@graph": listings
-      };
-      res.json(result);
+		Promise.all(files.map(readFile)).then(function(listings) {
+			var result = {
+				"@context": CONTEXT_URI,
+				"@graph": listings
+			};
+			res.json(result);
 
-      console.log("Done reading files. returning " + result);
+			console.log("Done reading files. returning " + result);
 
-    }).catch(function(err){
-      console.log( "readdir failed.", err);
-    });
-  });
+		}).catch(function(err){
+			console.log( "readdir failed.", err);
+		});
+	});
 });
 
 
 daemon.listen(config.listenOn, function(err){
-  console.log('listening on:', config.listenOn);
+	console.log('listening on:', config.listenOn);
 });
